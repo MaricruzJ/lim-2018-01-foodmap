3646b8c239ea0865fc3511d12f90742357e09070
MaricruzJ <maricruzj.er@gmail.com>
2018-08-15 12:01:03 -0500

    entrar

--- main/data.js
+++ main/data.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-    console.log('hola!');
     setTimeout(() => {
         contentSplash.classList.add('hiden');
         contentStart.classList.remove('hiden');
@@ -17,29 +16,8 @@ getJson = (url, callback) => {
         });
 }
 
-showRestaurantInZone = (objectRestaurant) => {
-
-    let cardRestaurant = document.createElement('div');
-    cardRestaurant.setAttribute('class', 'filtered');
-
-    let inputRestaurant = document.createElement('button');
-    inputRestaurant.setAttribute('class', 'restaurante-filtered');
-    inputRestaurant.style.backgroundImage = "url('" + objectRestaurant.photo + "')";
-    inputRestaurant.style.backgroundSize = "200px";
-
-    let nameRestaurant = document.createElement('p');
-    let textName = document.createTextNode(objectRestaurant.name);
-
-    cardRestaurant.appendChild(inputRestaurant);
-    nameRestaurant.appendChild(textName);
-    cardRestaurant.appendChild(nameRestaurant);
-
-    listFiltered.appendChild(cardRestaurant);
-}
-
-filterRestaurant = (objectRestaurant, filterNameRestaurant) => {
-    console.log(objectRestaurant.name);
-    let result = objectRestaurant.filter(restaurant => (restaurant.name.includes(filterNameRestaurant.toUpperCase())));
-    console.log(result);
+filterRestaurant = (jsonRestaurant, filterNameRestaurant) => {
+    
+    let result = jsonRestaurant.filter(restaurant => (restaurant.name.includes(filterNameRestaurant.toUpperCase())));
     return result;
 }
--- main/index.js
+++ main/index.js
@@ -6,12 +6,9 @@ const selectCity = document.getElementById('select-city');
 const btnSearchRestaurant = document.getElementById('btn-search-restaurant');
 
 const filterNameRestaurant = document.getElementById('filter-name-restaurant');
-const vegetarianFood = document.getElementById('vegetarian-food');
 const italianFood = document.getElementById('italian-food');
 const japanesseFood = document.getElementById('japanesse-food');
 const peruvianFood = document.getElementById('peruvian-food');
-const fastFood = document.getElementById('fast-food');
-const otherFood = document.getElementById('other-food');
 
 const listFiltered = document.getElementById('list-filtered');
 
@@ -32,26 +29,37 @@ getJson('../data/zone.json', (jsonZone) => {
     }
 
     btnSearchRestaurant.addEventListener('click', () => {
-        console.log('click en botón de busqueda de restaurant');
         contentSplash.classList.add('hiden');
         contentStart.classList.add('hiden');
         contentMain.classList.remove('hiden');
 
         getJson('../data/data.json', (jsonRestaurant) => {
-            console.log(jsonRestaurant);
 
             for (const keyRestaurant in jsonRestaurant) {
-                if (jsonRestaurant.hasOwnProperty(keyRestaurant)) {
-                    const objectRestaurant = jsonRestaurant[keyRestaurant];
-                    showRestaurantInZone(objectRestaurant);
 
-                    filterNameRestaurant.addEventListener('keyup', () => {
-                        
-                        let filterResult = filterRestaurant(objectRestaurant, filterNameRestaurant.value);
-                        console.log(filterResult);
-                    });
-                    
-                }
+                const restaurant = jsonRestaurant[keyRestaurant];
+                 showRestaurantInZone(restaurant);
+
+                filterNameRestaurant.addEventListener('keyup', () => {
+
+                    let filterResult = filterRestaurant(jsonRestaurant, filterNameRestaurant.value);
+                    console.log(filterResult);
+                });
+
+                italianFood.addEventListener('click', () => {
+                    listFiltered.innerHTML = '';
+                    showRestaurantItalian(restaurant);
+                })
+
+                japanesseFood.addEventListener('click', () => {
+                    listFiltered.innerHTML = '';
+                    showRestaurantJapanesse(restaurant);
+                })
+
+                peruvianFood.addEventListener('click', () => {
+                    listFiltered.innerHTML = '';
+                    showRestaurantPeruvian(restaurant);
+                })
             }
         })
     });
@@ -61,3 +69,93 @@ selectCity.addEventListener('change', () => {
     console.log(selectCity.value);
 });
 
+
+showRestaurantInZone = (objectRestaurant) => {
+
+    let cardRestaurant = document.createElement('div');
+    cardRestaurant.setAttribute('class', 'filtered');
+
+    let inputRestaurant = document.createElement('button');
+    inputRestaurant.setAttribute('class', 'restaurante-filtered');
+    inputRestaurant.style.backgroundImage = "url('" + objectRestaurant.photo + "')";
+    inputRestaurant.style.backgroundSize = "200px";
+
+    let nameRestaurant = document.createElement('p');
+    let textName = document.createTextNode(objectRestaurant.name);
+
+    cardRestaurant.appendChild(inputRestaurant);
+    nameRestaurant.appendChild(textName);
+    cardRestaurant.appendChild(nameRestaurant);
+
+    listFiltered.appendChild(cardRestaurant);
+}
+
+
+showRestaurantItalian = (objectRestaurant) => {
+
+    if (objectRestaurant.foodType === 'Comida Italiana') {
+        let cardRestaurant = document.createElement('div');
+        cardRestaurant.setAttribute('class', 'filtered');
+
+        let inputRestaurant = document.createElement('button');
+        inputRestaurant.setAttribute('class', 'restaurante-filtered');
+        inputRestaurant.style.backgroundImage = "url('" + objectRestaurant.photo + "')";
+        inputRestaurant.style.backgroundSize = "200px";
+
+        let nameRestaurant = document.createElement('p');
+        let textName = document.createTextNode(objectRestaurant.name);
+
+        cardRestaurant.appendChild(inputRestaurant);
+        nameRestaurant.appendChild(textName);
+        cardRestaurant.appendChild(nameRestaurant);
+
+        listFiltered.appendChild(cardRestaurant);
+    }
+
+}
+
+showRestaurantJapanesse = (objectRestaurant) => {
+  
+    if (objectRestaurant.foodType === 'Comida Japonesa') {
+        let cardRestaurant = document.createElement('div');
+        cardRestaurant.setAttribute('class', 'filtered');
+
+        let inputRestaurant = document.createElement('button');
+        inputRestaurant.setAttribute('class', 'restaurante-filtered');
+        inputRestaurant.style.backgroundImage = "url('" + objectRestaurant.photo + "')";
+        inputRestaurant.style.backgroundSize = "200px";
+
+        let nameRestaurant = document.createElement('p');
+        let textName = document.createTextNode(objectRestaurant.name);
+
+        cardRestaurant.appendChild(inputRestaurant);
+        nameRestaurant.appendChild(textName);
+        cardRestaurant.appendChild(nameRestaurant);
+
+        listFiltered.appendChild(cardRestaurant);
+    }
+
+}
+
+showRestaurantPeruvian = (objectRestaurant) => {
+   
+    if (objectRestaurant.foodType === 'Comida Peruana') {
+        let cardRestaurant = document.createElement('div');
+        cardRestaurant.setAttribute('class', 'filtered');
+
+        let inputRestaurant = document.createElement('button');
+        inputRestaurant.setAttribute('class', 'restaurante-filtered');
+        inputRestaurant.style.backgroundImage = "url('" + objectRestaurant.photo + "')";
+        inputRestaurant.style.backgroundSize = "200px";
+
+        let nameRestaurant = document.createElement('p');
+        let textName = document.createTextNode(objectRestaurant.name);
+
+        cardRestaurant.appendChild(inputRestaurant);
+        nameRestaurant.appendChild(textName);
+        cardRestaurant.appendChild(nameRestaurant);
+
+        listFiltered.appendChild(cardRestaurant);
+    }
+
+}
